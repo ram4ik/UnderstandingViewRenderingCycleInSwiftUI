af78ad22f58a766498b9bf9c20a60e93488725f1
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-09-26 21:43:59 +0300

    Initial Commit

---
+++ UnderstandingViewRenderingCycleInSwiftUI/UnderstandingViewRenderingCycleInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderstandingViewRenderingCycleInSwiftUIApp.swift
+//  UnderstandingViewRenderingCycleInSwiftUI
+//
+//  Created by Ramill Ibragimov on 26.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct UnderstandingViewRenderingCycleInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
